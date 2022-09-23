897f04be6676be29a3551a31535d9374e21675ca
Yvanoff <petruzzi.yvan@gmail.com>
2022-09-23 15:40:00 +0200

    included webserv.hpp in main

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/webserv.hpp"
+
 int	main(int argc, char *argv[], char *envp[])
 {
 	(void)argc;
