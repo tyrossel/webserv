7081ce6e76383202312eb2e6de262f7fcc83457d
Tanguy Rossel <trossel@c1r4s3.42lausanne.ch>
2022-09-26 17:13:09 +0200

    WIP: config parsing

---
+++ src/conf/parse.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trossel <trossel@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/26 16:47:18 by trossel           #+#    #+#             */
+/*   Updated: 2022/09/26 16:53:06 by trossel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parse.hpp"
+
+ConfigParsor::ConfigParsor(const std::string &filename)
+	: _filename(filename)
+{
+}
+
+ConfigParsor::~ConfigParsor()
+{
+}
+
+ConfigParsor::ConfigParsor(const ConfigParsor &rhs)
+{
+	*this = rhs;
+}
+
+ConfigParsor &ConfigParsor::operator=(const ConfigParsor &rhs)
+{
+	return (*this);
+}
+
+ServerConfig ConfigParsor::parse() const
+{
+	ServerConfig sc;
+
+	return sc;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,12 @@
 /*   By: trossel <trossel@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 15:21:50 by trossel           #+#    #+#             */
-/*   Updated: 2022/09/20 15:23:05 by trossel          ###   ########.fr       */
+/*   Updated: 2022/09/26 17:01:11 by trossel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Config.hpp"
+#include "conf/parse.hpp"
 
 void createConfig()
 {
@@ -30,12 +31,33 @@ void createConfig()
     std::cout << config << std::endl;
 }
 
+ServerConfig getConfig(const std::string &filename)
+{
+	ConfigParsor parsor(filename);
+	try
+	{
+		ServerConfig conf = parsor.parse();
+		return conf;
+
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+}
+
 int	main(int argc, char *argv[], char *envp[])
 {
 	(void)argc;
 	(void)argv;
 	(void)envp;
 
+	if (argc < 2 || argc > 2)
+	{
+		std::cerr << "Usage: ./webserv <config file>" << std::endl;
+		exit(1);
+	}
+	ConfigParsor parsor("
     createConfig();
 
 	return (0);
